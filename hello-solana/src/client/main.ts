57246749a774a7d22b299ffe5b6262b456f3ddd0
Jeongho Park <d0d0lbangi13@gmail.com>
2023-04-20 13:10:02 +0900

    FEAT: put keypair to create the on-chain Rust program

--- hello-solana/src/client/main.ts
+++ hello-solana/src/client/main.ts
@@ -10,3 +10,12 @@ import {
   import fs from 'mz/fs';
   import path from 'path';
   
+/*
+  Our keypair we used to create the on-chain Rust program
+*/
+const PROGRAM_KEYPAIR_PATH = path.join(
+    path.resolve(__dirname, '../../dist/program'),
+    'hello_solana-keypair.json'
+  );
+
+  
